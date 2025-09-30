3971c1059e52a46bceb08a2361d108447b1202c9
Maria Roukia Hamoudi <153323024+mariroka10@users.noreply.github.com>
2025-09-30 14:54:55 +0100

    Update search.py

--- search.py
+++ search.py
@@ -1,46 +1,37 @@
 
-# CHAIMA TALHI 
-
-import util #which contains utility classes and functions for the search algorithms.
-
+import util 
 class SearchProblem:
-    # returns the start state of the search problem.
     def getStartState(self):
         pass
-    #returns True if the state is a goal state, False otherwise.
     def isGoalState(self, state):
         pass
-    #It takes a state as input and returns a list of successor states, actions, and step costs from that state.    
     def expand(self, state):
         pass
-    #It takes a list of actions as input and returns the total cost of the action sequence.
     def getCostOfActionSequence(self, actions):
         pass
 
 def depthFirstSearch(prblm):
-    exploreed = set() #empty set to store visited states
-    borders = util.Stack() #to implement the frontier for the search algorithm.
+    exploreed = set() 
+    borders = util.Stack()
     
-    firstStartState = prblm.getStartState()# the start state of the problem is obtained using the getStartState method
-    firstStartNode = (firstStartState, []) #created with the start state and an empty list of actions.
+    firstStartState = prblm.getStartState()
+    firstStartNode = (firstStartState, []) 
 
-    borders.push(firstStartNode) #The start node is pushed onto the frontier stack
+    borders.push(firstStartNode) 
 
     while not borders.isEmpty():
-        actualState, path = borders.pop() # node is popped from the frontier, and its state and actions are extracted.
-        #If the current state is a goal state, the list of actions is returned as the solution.
+        actualState, path = borders.pop()
         if prblm.isGoalState(actualState):
             return path
 
         if actualState not in exploreed:
-            exploreed.add(actualState) #If the current state has not been visited, it is marked as visited
-            child = prblm.expand(actualState) # and the successors of the current state are expanded using the expand method
-            #. For each successor
+            exploreed.add(actualState) 
+            child = prblm.expand(actualState) 
             for nextState, action, _ in child:
-                newActions = path + [action] #a new list of actions is created by appending the current action,
-                borders.push((nextState, newActions)) #and the successor node is pushed onto the frontier stack.
+                newActions = path + [action] 
+                borders.push((nextState, newActions)) 
 
-    return path #If no goal state is found, the list of actions is returned as the solution.
+    return path
 
 def breadthFirstSearch(problem):
     visited = set()
@@ -69,17 +60,16 @@ def breadthFirstSearch(problem):
     return actions
 
 def uniformCostSearch(problem):
-    exploreed = set() #empty set
-    borders = util.PriorityQueue() #file fifo
+    exploreed = set()
+    borders = util.PriorityQueue() 
 
     firstStartState = problem.getStartState()
-    # a start node is created with the start state, an empty list of actions, and a cost of 0
-    firstStartNode = (firstStartState, [], 0)  # Include the cost in the start node tuple
+   
+    firstStartNode = (firstStartState, [], 0)  
 
-    borders.push(firstStartNode, 0)  # Initialize the priority with the cost
+    borders.push(firstStartNode, 0)  
 
     while not borders.isEmpty():
-        #a node is popped from the frontier, and its state, actions, and cost are extracted.
         actualState, path, cost = borders.pop()
 
         if problem.isGoalState(actualState):
@@ -91,25 +81,19 @@ def uniformCostSearch(problem):
 
             for nextState, action, stepCost in child:
                 newActions = path + [action]
-                newCost = cost + stepCost #The cost of the successor is updated by adding the step cost to the current cos
-                borders.push((nextState, newActions, newCost), newCost) # the successor node, along with its new actions and cost,
-                # is pushed onto the frontier priority queue with a priority equal to the new cost
-                # This ensures that nodes with lower costs are explored first.
+                newCost = cost + stepCost
+                borders.push((nextState, newActions, newCost), newCost)
 
     return path
 
-def aStarSearch(problem, hrstc=lambda state, problem: 0): #takes a problem and an optional heuristic function as input
+def aStarSearch(problem, hrstc=lambda state, problem: 0): 
     exploreed = set()
     borders = util.PriorityQueue()
 
     firstStartState = problem.getStartState()
-    # the start state, an empty list of actions, and a cost of 0
-    firstStartNode = (firstStartState, [], 0)  # Include the cost in the start node tuple
-    #The heuristic for the start state is calculated by calling 
-    #the heuristic function with the start state and the problem as arguments
-    startHeuristic = hrstc(firstStartState, problem)  # Calculate the heuristic for the start state
-    #the sum of the cost and the heuristic f= h+g
-    borders.push(firstStartNode, startHeuristic) #This ensures that nodes with lower costs and more promising heuristic values are explored first.
+    firstStartNode = (firstStartState, [], 0)  
+    startHeuristic = hrstc(firstStartState, problem)  #
+    borders.push(firstStartNode, startHeuristic) 
 
     while not borders.isEmpty():
         actualState, path, cost = borders.pop()
@@ -120,22 +104,13 @@ def aStarSearch(problem, hrstc=lambda state, problem: 0): #takes a problem and a
         if actualState not in exploreed:
             exploreed.add(actualState)
             succ = problem.expand(actualState) 
-            # the successors of the current state are expanded using the expand() method of the problem.
+           
             for nextState, action, stepCost in succ:
-                newActions = path + [action] #For each successor, a new list of actions is created by appending the current action to the existing actions.
-                newCost = cost + stepCost    #cost men problem 
+                newActions = path + [action] 
+                newCost = cost + stepCost   
                 
-                newHrstc = hrstc(nextState, problem)  # Calculate the heuristic for the successor state
-                # The heuristic for the successor state is calculated by calling 
-                # the heuristic function with the successor state and the problem as arguments
-                prio = newCost + newHrstc #The priority of the successor is calculated as the sum of the new cost and the new heuristic
-                borders.push((nextState, newActions, newCost), prio) #pushed onto the frontier priority queue with the calculated priority.
+                newHrstc = hrstc(nextState, problem)  #
+                prio = newCost + newHrstc 
+                borders.push((nextState, newActions, newCost), prio)
 
     return path
-
-#The breadthFirstSearch, uniformCostSearch, and aStarSearch functions follow a 
-# similar structure to the depthFirstSearch function, but use a different data structure 
-# for the frontier (queue for BFS, priority queue for UCS and A*) 
-# and may consider the cost and heuristic values when expanding and prioritizing nodes.
-
-# CHAIMA TALHI
